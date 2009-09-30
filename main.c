7290e05f188ee0bbecdeb7be5064d14793ebdbe5
Stefano Canepa <sc@orso.stefanocanepa.home>
2009-09-30 22:08:27 +0200

    Project cleanup before commit to githib

--- main.c
+++ main.c
@@ -1,35 +1,3 @@
-
-/* 
- * main.c --- short description
- * 
- * Copyright  (C)  2009  Stefano Canepa <sc@linux.it>
- * 
- * Version: 1.0
- * Keywords: 
- * Author: Stefano Canepa <sc@linux.it>
- * Maintainer: Stefano Canepa <sc@linux.it>
- * URL: http://
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY	; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Commentary: 
- * 
- * 
- * 
- * Code:
- */
-
 /* 
  * apt-spy (c) Steven Holmes, 2003. 
  * Stefano Canepa <sc@linux.it>, 2008, 2009
